83068f8772457c90d551a4a3814b91853447c530
Herbson Silva <herbsonsilvajp@gmail.com>
2023-07-03 02:14:42 -0300

    Adicionando Entidades

---
+++ Churras/Empresa.Churras.Domain.Model/Entities/Colega.cs
@@ -0,0 +1,9 @@
+﻿using Kernel.Domain.Model.Entities;
+
+namespace Empresa.Churras.Domain.Model.Entities;
+
+public class Colega : EntityKeySeq
+{
+    public string? Nome { get; private set; }
+    public string? Endereco { get; private set; }
+}
---
+++ Churras/Empresa.Churras.Domain.Model/Entities/Evento.cs
@@ -0,0 +1,13 @@
+﻿using Kernel.Domain.Model.Entities;
+
+namespace Empresa.Churras.Domain.Model.Entities;
+
+public class Evento : EntityKeySeq
+{
+    public string? DonoDaCasa { get; private set; }
+    public string? TipoEvento { get; private set; }
+    public DateTimeOffset Dia { get; private set; }
+    public string? Periodo { get; private set; }
+    public string? ColegasQueConfirmaramPresenca { get; private set; }
+    public string? OQueCadaColegaVaiLevar { get; private set; }
+}
