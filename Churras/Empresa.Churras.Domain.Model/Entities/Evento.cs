43f8891a3786bc5cd7fe5794792898df20aa8f77
Herbson Silva <herbsonsilvajp@gmail.com>
2023-07-06 00:17:03 -0300

    Adicionando Enums

--- Churras/Empresa.Churras.Domain.Model/Entities/Evento.cs
+++ Churras/Empresa.Churras.Domain.Model/Entities/Evento.cs
@@ -1,10 +1,12 @@
-﻿namespace Empresa.Churras.Domain.Model.Entities;
+﻿using Empresa.Churras.Domain.Model.Enums;
+
+namespace Empresa.Churras.Domain.Model.Entities;
 
 public class Evento : EntityKeySeq
 {
     public long DonoDaCasaKey { get; private set; }
     public Colega DonoDaCasa { get; private set; }
-    public string TipoEvento { get; private set; }
+    public TipoEvento Tipo { get; private set; }
     public DateTimeOffset Dia { get; private set; }
     public Periodo Periodo { get; private set; }
     public ICollection<EventoColegaConfirmado> ColegasConfirmados { get; private set; }
